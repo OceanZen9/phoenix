db1bff31ebb2e5280840a80eb912b36324a4d779
fgloris <stepthenfriedman@gmail.com>
2025-09-28 15:24:38 +0800

    feat
    add shadecn dependency

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,9 +4,13 @@
  * - 首页 推荐榜单 商品分类栏
  * - 推荐商品展示
  */
+import { Button } from "@/components/ui/button"
+
+
 function HomePage() {
   return (
     <div>
+      <Button>Click me</Button>
       <h1>欢迎来到首页</h1>
       <p>这里是凤凰商城的主页。</p>
     </div>
