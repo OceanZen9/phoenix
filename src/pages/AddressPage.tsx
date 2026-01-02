797afe0a2d789e12cfcbde802b254b673ee6044a
Haiyang Zeng <1528285025@qq.com>
2026-01-02 21:48:57 +0800

    fix: remove unused CardTitle and CardDescription imports

--- src/pages/AddressPage.tsx
+++ src/pages/AddressPage.tsx
@@ -11,10 +11,8 @@ import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
-  CardTitle,
 } from "@/components/ui/card";
 import {
   Dialog,
